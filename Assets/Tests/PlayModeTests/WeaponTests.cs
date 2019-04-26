088dcc8f859ef9430dcc289c43d56e89774420c2
Kbob <lunchtablestudio@gmail.com>
2019-04-25 22:11:33 -0400

    Added weapontests, removed TEstEdit

---
+++ Assets/Tests/PlayModeTests/WeaponTests.cs
@@ -0,0 +1,15 @@
+namespace Tests
+{
+    using System.Collections;
+    using NUnit.Framework;
+    using UnityEngine;
+    using UnityEngine.TestTools;
+    using Unity.Entities;
+
+    [ TestFixture ]
+    [ Category( "Asteroids Tests" ) ]
+    public class WeaponTests : ECSTestFixture
+    {
+        
+    }
+}
